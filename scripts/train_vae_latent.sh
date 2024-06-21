8ec0c2ab6467a2c2bb6342b5485879d4d5419581
quattrinifabio <fabioquattrini35@gmail.com>
2024-06-21 15:34:47 +0200

    train_vae_latent sh

--- scripts/train_vae_latent.sh
+++ scripts/train_vae_latent.sh
@@ -4,14 +4,14 @@
 #SBATCH --partition=all_usr_prod
 #SBATCH -e /work/FoMo_AIISDH/scascianelli/2024_emuru/jobs/train_vae_latent_%j.err
 #SBATCH -o /work/FoMo_AIISDH/scascianelli/2024_emuru/jobs/train_vae_latent_%j.out
-#SBATCH --mem=60G
+#SBATCH --mem=80G
 #SBATCH --constraint="gpu_A40_48G|gpu_RTX6000_24G|gpu_RTXA5000_24G"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1              
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --time=1-00:00:00
-#SBATCH -J vae_latent_trainings
+#SBATCH -J vae_latent_training
 #SBATCH --array=0-5%1
 
 . /usr/local/anaconda3/etc/profile.d/conda.sh
@@ -20,14 +20,14 @@ conda activate emuru
 cd /work/FoMo_AIISDH/fquattrini/Emuru
 
 export OMP_NUM_THREADS=16
-export SCRIPT=train_vae.py
+export SCRIPT=train_vae_latent.py
 
 export SCRIPT_ARGS=" \
     --output_dir /work/FoMo_AIISDH/scascianelli/2024_emuru/results_vae_latent \
     --logging_dir /work/FoMo_AIISDH/scascianelli/2024_emuru/results_vae_latent \
     --latent_htr_wid True \
-    --train_batch_size 32 \
-    --resume_id vvv0 \
+    --train_batch_size 4 \
+    --resume_id vle0 \
     "
 
 if [[ $SLURM_ARRAY_TASK_ID  -eq 0 ]]; then
